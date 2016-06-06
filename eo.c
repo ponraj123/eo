4f08196647eb11a893627956e8b4ecc912ac6967
ponraj123 <ponraj.bluesky@gmail.com>
2016-06-06 16:15:50 +0530

    Create eo.c

---
+++ eo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf(" the no is even:");
+	}
+	else
+	{
+		printf(" the no is odd:");
+	}
+	// your code goes here
+	return 0;
+}
